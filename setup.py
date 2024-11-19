4c5781ed106eee8afe07f0a676499e7395ea737b
ChangSuChoi <choics2623@gmail.com>
2024-11-19 20:30:37 +0900

    for pakaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vla_recipes',
+    version='0.1.0',
+    description='VLA Recipes for Finetuning and Training',
+    author='ChangSu Choi',
+    author_email='choics2623@gmail.com',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'accelerate',
+        'transformers',
+        'datasets',
+        'numpy',
+        'wandb',
+    ],
+    python_requires='>=3.6',
+)
