940a4dc8083e993af4ce03f5041b572264477092
ChangSuChoi <choics2623@gmail.com>
2024-12-08 17:06:23 +0900

    라이브러리 설명 관련 조금 수정

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # vla_recipes
+개발중인 라이브러리  
+```
+pip install -e .
+```
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='vla_recipes',
     version='0.1.0',
-    description='VLA Recipes for Finetuning and Training',
+    description='VLA Recipes for Finetuning and Training (Included LLM, VLM Training)',
     author='ChangSu Choi',
     author_email='choics2623@gmail.com',
     packages=find_packages(where='src'),
